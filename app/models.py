90482cc97a2b3d25cbefa1f23c045f7defa274c6
Nipurn <nipurnbandi30@gmail.com>
2026-03-25 23:04:25 +0530

    votes table established

--- app/models.py
+++ app/models.py
@@ -30,3 +30,11 @@ class Users(Base):
     email=Column(String,nullable=False,unique=True)
     password=Column(String,nullable=False)
     created_at=Column(TIMESTAMP,server_default=text('now()'),nullable=False)
+
+
+
+
+class Votes(Base):
+    __tablename__="votes"
+    user_id=Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),primary_key=True,nullable=False)
+    post_id=Column(Integer,ForeignKey("post.id",ondelete="CASCADE"),primary_key=True,nullable=False,)
